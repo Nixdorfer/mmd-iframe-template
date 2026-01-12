3a68147aa0ea7c1fb4076019ba77101d872a1b64
Nix <admin@nixdorfer.com>
2026-01-12 18:26:35 +0100

    sync

--- template/modal/v2/library/loader.js
+++ template/modal/v2/library/loader.js
@@ -11,7 +11,7 @@ var c=pending.length;
 pending.forEach(function(n){
 self.loaded[n]=1;
 var s=document.createElement('scr'+'ipt');
-s.src=self.base+'library/'+n+'.js?t='+Date.now();
+s.src=self.base+'template/modal/v2/library/'+n+'.js?t='+Date.now();
 s.onload=function(){if(--c===0&&cb)cb()};
 document.head.appendChild(s);
 });
@@ -34,10 +34,10 @@ var idx=nav.projects[L.project]?.index;
 if(!idx){document.body.innerHTML='<div style="padding:16px;background:#8b0000;border-radius:8px;color:#fff">未找到: '+L.project+'</div>';return}
 fetch(L.base+idx+'?t='+Date.now(),{cache:'no-store'}).then(function(r){return r.text()}).then(function(h){
 console.log('[loader] project:',h);
-var cfg='<scr'+'ipt>window.L='+JSON.stringify({loaded:{},base:L.base,edit:L.edit,project:L.project,char:L.char,user:L.user,b64:L.b64,res:L.res,platform:L.platform})+';L.load=function(libs,cb){var self=this,pending=libs.filter(function(n){return!self.loaded[n]});if(!pending.length)return cb&&cb();var c=pending.length;pending.forEach(function(n){self.loaded[n]=1;var s=document.createElement("scr"+"ipt");s.src=self.base+"library/"+n+".js?t="+Date.now();s.onload=function(){if(--c===0&&cb)cb()};document.head.appendChild(s)})};'+'</scr'+'ipt>';
+var cfg='<scr'+'ipt>window.L='+JSON.stringify({loaded:{},base:L.base,edit:L.edit,project:L.project,char:L.char,user:L.user,b64:L.b64,res:L.res,platform:L.platform})+';L.load=function(libs,cb){var self=this,pending=libs.filter(function(n){return!self.loaded[n]});if(!pending.length)return cb&&cb();var c=pending.length;pending.forEach(function(n){self.loaded[n]=1;var s=document.createElement("scr"+"ipt");s.src=self.base+"template/modal/v2/library/"+n+".js?t="+Date.now();s.onload=function(){if(--c===0&&cb)cb()};document.head.appendChild(s)})};'+'</scr'+'ipt>';
 console.log('[loader] cfg:',cfg);
 if(L.edit){
-fetch(L.base+'library/editor.html?t='+Date.now(),{cache:'no-store'}).then(function(r){return r.text()}).then(function(ed){
+fetch(L.base+'template/modal/v2/library/editor.html?t='+Date.now(),{cache:'no-store'}).then(function(r){return r.text()}).then(function(ed){
 console.log('[loader] editor:',ed);
 document.open();document.write(cfg+h+ed);document.close();
 }).catch(function(e){console.log('[loader] editor error:',e);document.open();document.write(cfg+h);document.close()});
